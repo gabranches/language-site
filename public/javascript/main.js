ef4b131a31e102d2303f7f38e42f905bdae758ae
Gustavo Abranches <gabranches@gmail.com>
2016-12-19 14:48:01 -0500

    added Sentence prototype

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -29,37 +29,60 @@ $(function () {
     }
 
     var things = [
-        { word: 'cup', translation: '컵' },
-        { word: 'book', translation: '책' },
-        { word: 'computer', translation: '컴퓨터' },
-        { word: 'bag', translation: '가방컵' }
+        { w: 'cup', t: '컵' },
+        { w: 'book', t: '책' },
+        { w: 'computer', t: '컴퓨터' },
+        { w: 'bag', t: '가방' }
     ];
 
     function Sentence(structure) {
+
+        this.varsOriginal = [];
+        this.varsTranslated = [];
 
         this.variables = structure.variables;
         this.original = structure.original;
         this.translation = structure.translation;
 
+        this.genOriginal = function () {
+            return this.original.format(this.varsOriginal);
+        }
+
+        this.genTranslation = function () {
+            return this.translation.format(this.varsTranslated);
+        }
+
+        this.buildVarArrays = (function () {
+            for (i = 0; i < this.variables.length; i++) {
+                this.varsOriginal.push(this.variables[i].w)
+                this.varsTranslated.push(this.variables[i].t)
+            }
+        }).apply(this);
+
     }
 
     var sentences = [
         {
-            name: '"Where is my" question',
             variables: [new Word().noun.thing()],
-            original: function () {
-                return ['Where', 'is', 'my', this.variables[0].word, '?']
-            },
-            translation: function () {
-                return ['제', this.variables[0].translation, '어디입니까?']
-            }
+            original: 'Where is my {0}?',
+            translation: '제 {0} 어디입니까?'
         }
     ];
+
 
     var sentence = new Sentence(pickRandom(sentences));
 
 
-    $("#sentence").text(sentence.original().join(' '));
-    console.log(sentence.translation());
+    String.prototype.format = function () {
+        var formatted = this;
+        for (var i = 0; i < arguments.length; i++) {
+            var regexp = new RegExp('\\{' + i + '\\}', 'gi');
+            formatted = formatted.replace(regexp, arguments[i]);
+        }
+        return formatted;
+    };
+
+    $("#sentence").text(sentence.genOriginal());
+    console.log(sentence.genTranslation());
 
 });
