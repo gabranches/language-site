14005a4b370ec165a8e6bf6b730def32f31f7620
Gustavo Abranches <gabranches@gmail.com>
2016-12-21 12:20:49 -0500

    added basic interface

--- models/word-set.js
+++ models/word-set.js
@@ -1,8 +1,13 @@
 var mongoose = require('mongoose');
 
+var wordSchema = mongoose.Schema({
+    w: String, 
+    t: String 
+});
+
 var wordsetSchema = mongoose.Schema({
     name: { type: String, unique: true, index: true },
-    words: [{ w: String, t: String }]
+    words: [wordSchema]
 });
 
 var WordSet = mongoose.model('wordSet', wordsetSchema);
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,35 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var env = require('../env');
-
-mongoose.connect('mongodb://'+env.user+':'+env.pass+'@ds133428.mlab.com:33428/language-site');
+var db = require('../lib/db')
+var WordSet = require('../models/word-set');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Language Site' });
 });
+
+router.get('/wordsets', function (req, res, next) {
+    WordSet.find(function (err, doc) {
+        res.render('wordsets', { data: doc });
+    });
+});
+
+router.post('/wordsets/add', function (req, res) {
+   db.createWordset(req.body.name, function() {
+       res.redirect('/wordsets');
+   });
+   
+});
+
+router.get('/wordset', function (req, res, next) {
+    res.render('wordset', { title: 'Language Site' });
+});
+
+router.get('/sentences', function (req, res, next) {
+    res.render('sentences', { title: 'Language Site' });
+});
+
+
 
 module.exports = router;
